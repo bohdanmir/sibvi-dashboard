bd54110d7d86ffa52605b1111c688b6fb8bfb037
bohdanmir <bohdan@mirinsight.com>
2025-08-27 10:47:24 +0100

    Add grey line on right side of this right side menu

--- components/ui/sidebar.tsx
+++ components/ui/sidebar.tsx
@@ -235,8 +235,8 @@ function Sidebar({
             : "right-0 group-data-[collapsible=offcanvas]:right-[calc(var(--sidebar-width)*-1)]",
           // Adjust the padding for floating and inset variants.
           variant === "floating" || variant === "inset"
-            ? "p-2 group-data-[collapsible=icon]:w-[calc(var(--sidebar-width-icon)+(--spacing(4))+2px)]"
-            : "group-data-[collapsible=icon]:w-(--sidebar-width-icon) group-data-[side=left]:border-r group-data-[side=right]:border-l",
+            ? "p-2 group-data-[collapsible=icon]:w-[calc(var(--sidebar-width-icon)+(--spacing(4))+2px)] border-r border-gray-200"
+            : "group-data-[collapsible=icon]:w-(--sidebar-width-icon) group-data-[side=left]:border-r border-gray-200 group-data-[side=right]:border-l border-gray-200",
           className
         )}
         {...props}
